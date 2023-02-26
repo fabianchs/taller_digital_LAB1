eaeb21d9049be3cd26f5b296baa6cf73b6ecb7b7
chackymeow <fabichasola@hotmail.com>
2023-02-26 21:19:58 +0000

    7 seg connected to FPGA

--- EJ4_DECO_7_SEGMENTS/seven_segments.sv
+++ EJ4_DECO_7_SEGMENTS/seven_segments.sv
@@ -1,38 +1,61 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.02.2023 18:29:05
+// Design Name: 
+// Module Name: seven_segments
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-//Este módulo de siete segmentos está basado en el libro "FPGA PROTOTYPING BY VERILOG EXAMPLES" de PONG P. CHU PAG.67
 
-module seven_segments
-    (
+module seven_segments(CLK,SS_LED, AN0, AN1,AN2,AN3,AN4,AN5,AN6,AN7, DP, sw0,sw1,sw2,sw3, b0,b2, LED);
+    input CLK;
+    input [3:0] sw0,sw1,sw2,sw3;
+    input AN1,AN2,AN3,AN4,AN5,AN6,AN7;
+    input logic b0,b2;
+    output AN0, DP;
+    output reg [6:0]SS_LED;
+    output reg [15:0]LED;
+    assign AN0=0;
+    assign {AN1,AN2,AN3,AN4,AN5,AN6,AN7}=0;
+    assign DP=1;
+    reg [3:0] send_switch;
+    //wire [6:0] send_sseg;
     
-    input wire [3:0]hex,
-    input wire dp,
-    output reg [7:0] seven_seg
-
-    );
+    always @(b0,b2) begin
     
-    always @*
-    begin
-        case(hex)
-            4'h0: seven_seg[6:0] = 7'b0000001;
-            4'h1: seven_seg[6:0] = 7'b1001111;
-            4'h2: seven_seg[6:0] = 7'b0010010;
-            4'h3: seven_seg[6:0] = 7'b0000110;
-            4'h4: seven_seg[6:0] = 7'b1001100;
-            4'h5: seven_seg[6:0] = 7'b0100100;
-            4'h6: seven_seg[6:0] = 7'b0100000;
-            4'h7: seven_seg[6:0] = 7'b0001111;
-            4'h8: seven_seg[6:0] = 7'b0000000;
-            4'h9: seven_seg[6:0] = 7'b0000100;
-            4'ha: seven_seg[6:0] = 7'b0001000;
-            4'hb: seven_seg[6:0] = 7'b1100000;
-            4'hc: seven_seg[6:0] = 7'b0110001;
-            4'hd: seven_seg[6:0] = 7'b1000010;
-            4'he: seven_seg[6:0] = 7'b0110000;
+        if(b0==0 & b2==0)begin
+            send_switch=sw0;
             
-           default seven_seg[6:0]=7'b0111000;
-        endcase
-        seven_seg[7]=dp; //El MSB está conectado a dp
+            LED=16'b0000000000001111;
+        end else if (b0==0 & b2==1) begin
+            send_switch=sw1;
+            LED=16'b0000000011110000;
+        end else if (b0==1 & b2==0) begin
+            send_switch=sw2;
+            LED=16'b0000111100000000;
+        end else if (b0==1 & b2==1) begin
+            send_switch=sw3;
+            LED=16'b1111000000000000;
+            
+        end
+       
     end
-           
+   
+
+    bin_to_ss b1(.hex(send_switch), .dp(DP), .seven_seg(SS_LED));
+
+    
 endmodule
